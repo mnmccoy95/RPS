471cb5f4abbd8b075dc493062f30656601704a57
mnmccoy95 <mnmccoy95@gmail.com>
2020-11-13 10:41:52 -0600

    game initializes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace RPS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int playerScore = 0;
+            int compScore = 0;
+            Play(playerScore, compScore);
+        }
+
+        static void Play(int playerScore, int compScore)
+        {
+            Console.WriteLine($@"
+            ---------------------------
+            | Player: {playerScore} | Computer: {compScore} |
+            ---------------------------
+            What would you like to throw?
+            1) Rock
+            2) Paper
+            3) Scissors
+            ");
         }
     }
 }
