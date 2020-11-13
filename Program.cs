85ebb30a6575fba70fb94d3788ddc18f9948fc8c
mnmccoy95 <mnmccoy95@gmail.com>
2020-11-13 13:37:32 -0600

    refactored

--- Program.cs
+++ Program.cs
@@ -13,106 +13,67 @@ namespace RPS
 
         static void Play(int playerScore, int compScore)
         {
+            Console.WriteLine($@"
+                ---------------------------
+                | Player: {playerScore} | Computer: {compScore} |
+                ---------------------------"
+            );
+
             if(playerScore != 3 && compScore != 3)
             {
-            Console.Write($@"
-            ---------------------------
-            | Player: {playerScore} | Computer: {compScore} |
-            ---------------------------
-            What would you like to throw?
-            1) Rock
-            2) Paper
-            3) Scissors
-            ");
+                Console.Write($@"
+                    What would you like to throw?
+                    1) Rock
+                    2) Paper
+                    3) Scissors
+                ");
 
-            int answer = Int32.Parse(Console.ReadLine());
-            int compAnswer = Number();
-
-            Compare(answer, compAnswer, playerScore, compScore);
+                int answer = Int32.Parse(Console.ReadLine());
+                int compAnswer = Number();
+                Compare(answer, compAnswer, playerScore, compScore);
             }
             else if (playerScore == 3)
             {
-                Console.WriteLine("You Win!");
+
+                Console.WriteLine(@"
+                    You win!"
+                );
             }
             else if (compScore == 3)
             {
-                Console.WriteLine("You Lose :(");
+                Console.WriteLine(@"
+                    You lose :("
+                );
             }
         }
-
         static int Number() {
             Random r = new Random();
             int genRand= r.Next(1,4);
             return genRand;
         }
-
         static void Compare(int playerAnswer, int compAnswer, int playerScore, int compScore)
         {
-            if(playerAnswer == 1)
-            {
-                RockPrint();
-                Console.WriteLine("vs");
-                if(compAnswer == 1)
-                {
-                    RockPrint();
-                }
-                if(compAnswer == 2)
-                {
-                    PaperPrint();
-                    compScore++;
-                }
-                if(compAnswer == 3)
-                {
-                    ScissorsPrint();
-                    playerScore++;
-                }
-            }
+            PrintASCII(playerAnswer);
+            Console.WriteLine(@"
+                vs."
+            );
+            PrintASCII(compAnswer);
 
-            if(playerAnswer == 2)
+            if(compAnswer == playerAnswer + 1 | compAnswer == playerAnswer - 2)
             {
-                PaperPrint();
-                Console.WriteLine("vs");
-                if(compAnswer == 1)
-                {
-                    RockPrint();
-                    playerScore++;
-                }
-                if(compAnswer == 2)
-                {
-                    PaperPrint();
-                }
-                if(compAnswer == 3)
-                {
-                    ScissorsPrint();
-                    compScore++;
-                }
+                compScore++;
             }
-
-            if(playerAnswer == 3)
+            else if(playerAnswer == compAnswer + 1 | playerAnswer == compAnswer - 2)
             {
-                ScissorsPrint();
-                Console.WriteLine("vs");
-                if(compAnswer == 1)
-                {
-                    RockPrint();
-                    compScore++;
-                }
-                if(compAnswer == 2)
-                {
-                    PaperPrint();
-                    playerScore++;
-                }
-                if(compAnswer == 3)
-                {
-                    ScissorsPrint();
-                }
+                playerScore++;
             }
 
             Play(playerScore, compScore);
         }
 
-        static void RockPrint()
+        static void PrintASCII(int hand)
         {
+            if(hand == 1){
             Console.WriteLine(@"
                     _______
                 ---'   ____)
@@ -121,11 +82,11 @@ namespace RPS
                         (____)
                 ---.__(___)            
             ");
-        }
+            }
 
-        static void PaperPrint()
-        {
-            Console.WriteLine(@"
+            if(hand == 2)
+            {
+                Console.WriteLine(@"
                     _______
                 ---'    ____)____
                             ______)
@@ -133,18 +94,19 @@ namespace RPS
                             _______)
                 ---.__________)            
             ");
-        }
+            }
 
-        static void ScissorsPrint()
-        {
-            Console.WriteLine(@"
-                _______
-            ---'   ____)____
-                        ______)
-                    __________)
-                    (____)
-            ---.__(___)            
-            ");
+            if(hand == 3)
+            {
+                Console.WriteLine(@"
+                    _______
+                ---'   ____)____
+                            ______)
+                        __________)
+                        (____)
+                ---.__(___)            
+                ");
+            }
         }
     }
 }
