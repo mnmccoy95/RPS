cdb1013542ea6d884a87720e8d365bf923515988
mnmccoy95 <mnmccoy95@gmail.com>
2020-11-13 11:02:44 -0600

    game works, but doesn't show you compy's throws

--- Program.cs
+++ Program.cs
@@ -13,7 +13,9 @@ namespace RPS
 
         static void Play(int playerScore, int compScore)
         {
-            Console.WriteLine($@"
+            if(playerScore != 3 && compScore != 3)
+            {
+            Console.Write($@"
             ---------------------------
             | Player: {playerScore} | Computer: {compScore} |
             ---------------------------
@@ -22,6 +24,59 @@ namespace RPS
             2) Paper
             3) Scissors
             ");
+
+            int answer = Int32.Parse(Console.ReadLine());
+            int compAnswer = Number();
+
+            Compare(answer, compAnswer, playerScore, compScore);
+            }
+        }
+
+        static int Number() {
+            Random r = new Random();
+            int genRand= r.Next(1,3);
+            return genRand;
+        }
+
+        static void Compare(int playerAnswer, int compAnswer, int playerScore, int compScore)
+        {
+            if(playerAnswer == 1)
+            {
+                if(compAnswer == 2)
+                {
+                    compScore++;
+                }
+                if(compAnswer == 3)
+                {
+                    playerScore++;
+                }
+            }
+
+            if(playerAnswer == 2)
+            {
+                if(compAnswer == 3)
+                {
+                    compScore++;
+                }
+                if(compAnswer == 1)
+                {
+                    playerScore++;
+                }
+            }
+
+            if(playerAnswer == 3)
+            {
+                if(compAnswer == 1)
+                {
+                    compScore++;
+                }
+                if(compAnswer == 2)
+                {
+                    playerScore++;
+                }
+            }
+
+            Play(playerScore, compScore);
         }
     }
 }
